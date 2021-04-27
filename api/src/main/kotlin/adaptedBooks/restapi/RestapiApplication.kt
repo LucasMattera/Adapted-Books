971092e0683ed1f18393fa44c66bfd19de16dd57
JeremiasFuentes <jeremiasfuentes77@gmail.com>
2021-04-26 21:18:21 -0300

    Agregados libros juego de tronos y expanse

--- api/src/main/kotlin/adaptedBooks/restapi/RestapiApplication.kt
+++ api/src/main/kotlin/adaptedBooks/restapi/RestapiApplication.kt
@@ -19,6 +19,45 @@ class RestapiApplication: CommandLineRunner {
 		var libros = mutableListOf<Libro>()
 		val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
 
+		val leviatanImagen = "https://m.media-amazon.com/images/I/51K3p2lbJeL.jpg"
+		val leviatanLinks = listOf<String>("https://www.primevideo.com/detail/0MW6F85MD8486AE43GONNOR5F1/ref=atv_dp_season_select_s1?language=es_ES")
+		val leviatan = Libro("El Despertar del Leviatan","James S. A. Corey", "Estados Unidos", leviatanImagen, leviatanLinks, LocalDate.parse("02-06-2011",formatter))
+		libros.add(leviatan)
+
+		val calibanImagen = "https://1.bp.blogspot.com/-iWQam0OWJNI/YBvjzjc-NSI/AAAAAAAAabU/uwHc9-dyZP4Mfayu6bE2aatdQ6UL1ly6ACNcBGAsYHQ/s2048/la%2Bguerra%2Bde%2Bcaliban.jpg"
+		val caliban = Libro("La Guerra de Caliban" ,"James S. A. Corey", "Estados Unidos", calibanImagen, leviatanLinks, LocalDate.parse("07-06-2012",formatter))
+		libros.add(caliban)
+
+		val abadonImagen = "https://m.media-amazon.com/images/I/519qqD2rVQL.jpg"
+		val abadon = Libro("La Puerta de Abadon" ,"James S. A. Corey", "Estados Unidos", abadonImagen, leviatanLinks, LocalDate.parse("04-06-2013",formatter))
+		libros.add(abadon)
+
+		val akiraImagen = "https://pics.filmaffinity.com/Akira-557684565-mmed.jpg"
+		val akiraLinks = listOf<String>("https://www.netflix.com/ar/title/60021103")
+		val akira = Libro("Akira", "Katsuhiro Otomo" , "Japon", akiraImagen, akiraLinks,LocalDate.parse("20-12-1982",formatter))
+  		libros.add(akira)
+
+		val cdhyf1Imagen = "https://contentv2.tap-commerce.com/cover/large/9789506442279_1.jpg?id_com=1113"
+		val cdhyfLinks = listOf<String>("https://www.hbolatam.com/uy/series/detail/game-of-thrones/13673/ttl603252")
+		val cdhyf1 = Libro("Juego de Tronos", "George RR Martin","Estados Unidos", cdhyf1Imagen,cdhyfLinks,LocalDate.parse("06-08-1996",formatter))
+		libros.add(cdhyf1)
+
+		val cdhyf2Imagen = "https://images.cdn1.buscalibre.com/fit-in/360x360/6f/30/6f305637f3f8c29af1f523fb302e7745.jpg"
+		val cdhyf2 = Libro("Choque de Reyes", "George RR Martin","Estados Unidos", cdhyf2Imagen,cdhyfLinks,LocalDate.parse("16-11-1998",formatter))
+		libros.add(cdhyf2)
+
+		val cdhyf3Imagen = "https://www.libreriasinopsis.com/imagenes/9788416/978841603507.JPG"
+		val cdhyf3 = Libro("Tormenta de Espadas", "George RR Martin","Estados Unidos", cdhyf3Imagen,cdhyfLinks,LocalDate.parse("08-08-2000",formatter))
+		libros.add(cdhyf3)
+
+		val cdhyf4Imagen = "https://contentv2.tap-commerce.com/cover/large/9789506442477_1.jpg?id_com=1113"
+		val cdhyf4 = Libro("Festín de Cuervos", "George RR Martin","Estados Unidos", cdhyf4Imagen,cdhyfLinks,LocalDate.parse("26-10-2005",formatter))
+		libros.add(cdhyf4)
+
+		val cdhyf5Imagen = "https://i.pinimg.com/originals/c9/a6/97/c9a697ae8ce5d49ce7a9e6aeb3f8d2f7.jpg"
+		val cdhyf5 = Libro("Danza de Dragones", "George RR Martin","Estados Unidos", cdhyf5Imagen,cdhyfLinks,LocalDate.parse("12-07-2011",formatter))
+		libros.add(cdhyf5)
+
 		val HPPiedraFilosofalImagen = "https://static.wikia.nocookie.net/esharrypotter/images/6/62/HP1_ES_versi%C3%B3n_Pottermore.jpg/revision/latest/scale-to-width-down/719?cb=20170711202411"
 		val HPPiedraFilosofalLinks = listOf<String>("https://www.youtube.com/watch?v=2BXHcqtFU8k")
 		val HPPiedraFilosofal = Libro("Harry Potter y la piedra filosofal","J. K. Rowling","Reino Unido",HPPiedraFilosofalImagen,HPPiedraFilosofalLinks, LocalDate.parse("30-06-1997",formatter))
