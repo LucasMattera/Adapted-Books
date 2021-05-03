ae2e81792a36c5a6d681ba8c5f271bc15c8cdc3d
franarci <fran.arciniega96@gmail.com>
2021-05-02 23:01:17 -0300

    componente ArticuloMiniatura creado

---
+++ web/adbooksweb-app/src/components/ArticuloMiniatura.js
@@ -0,0 +1,17 @@
+function ArticuloMiniatura ({libro, id, to}){
+    
+    const {titulo, imagen}= libro;       
+        return  (  
+          <div key = {id}>
+              <a href={imagen}>
+                      <img 
+                          className="imagen" 
+                          src={`${imagen}`} 
+                          alt={titulo}
+                      />
+              </a>
+          <p>{titulo}</p>
+          </div>)
+  }
+  
+  export default ArticuloMiniatura;
