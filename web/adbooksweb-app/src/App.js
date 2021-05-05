f40f85abfd69610427e33866df6268f8dcd83e4f
LucasMattera <lucasmattera97@gmail.com>
2021-05-05 15:28:35 -0300

    add navbar a la app

--- web/adbooksweb-app/src/App.js
+++ web/adbooksweb-app/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Inicio from './components/Inicio';
 import Libro from './components/Libro';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import './styles/App.css';
 
 class App extends React.Component {
@@ -13,16 +14,18 @@ class App extends React.Component {
         return (
             <div className="page-container">
 
+
                 <div className="content-wrap">
                     <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/" component={Inicio} /> 
-                        <Route path="/libros/" component={Libro}/>
-                        <Route path="*" render= {() =>
-                            <h1>Not Found</h1>
-                        }/>
-                    </Switch>
-                    </BrowserRouter>
+                        <Navbar/>   
+                        <Switch>
+                            <Route exact path="/" component={Inicio} /> 
+                            <Route path="/libros/" component={Libro}/>
+                            <Route path="*" render= {() =>
+                                <h1>Not Found</h1>
+                            }/>
+                        </Switch>
+                        </BrowserRouter>
                 </div>
 
                 <Footer/> 
