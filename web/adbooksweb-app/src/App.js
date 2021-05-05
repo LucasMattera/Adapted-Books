e7c1a1b1a7326b4213b83d9e4b79db98566cf90a
LucasMattera <lucasmattera97@gmail.com>
2021-05-05 13:03:47 -0300

    add divs en App y estilos en App para Footer

--- web/adbooksweb-app/src/App.js
+++ web/adbooksweb-app/src/App.js
@@ -3,22 +3,31 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Inicio from './components/Inicio';
 import Libro from './components/Libro';
+import Footer from './components/Footer';
+import './styles/AppK.css';
 
- class App extends React.Component {
+class App extends React.Component {
 
 
- render(){
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Inicio} /> 
-        <Route path="/libros/" component={Libro}/>
-        <Route path="*" render= {() =>
-            <h1>Not Found</h1>
-        }/>
-      </Switch>
-    </BrowserRouter>
-    )
-  }
+    render(){
+        return (
+            <div className="page-container">
+
+                <div className="content-wrap">
+                    <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={Inicio} /> 
+                        <Route path="/libros/" component={Libro}/>
+                        <Route path="*" render= {() =>
+                            <h1>Not Found</h1>
+                        }/>
+                    </Switch>
+                    </BrowserRouter>
+                </div>
+
+                <Footer/> 
+            </div>
+        )
+    }
 }
 export default App;
