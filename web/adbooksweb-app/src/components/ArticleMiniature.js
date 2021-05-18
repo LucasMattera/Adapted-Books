593fba59b31aee64a4f73ed80712281b290764ff
franarci <fran.arciniega96@gmail.com>
2021-05-18 00:52:31 -0300

    Añado estilos a ArticleMiniature

--- web/adbooksweb-app/src/components/ArticleMiniature.js
+++ web/adbooksweb-app/src/components/ArticleMiniature.js
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import "../styles/ArticleMiniature.css"
 
 function ArticleMiniature ({libro, id}){
     const history= useHistory()
@@ -9,16 +10,30 @@ function ArticleMiniature ({libro, id}){
     const {titulo, imagen}= libro;       
     
     return (  
-          <div key = {id}>
-              <a onClick= {handleClick}>
-                      <img 
-                          className="imagen" 
-                          src={imagen} 
-                          alt={titulo}
-                      />
-              </a>
-          <p>{titulo}</p>
-          </div>)
+    <div class="article-card">
+        <div class="article-card-inner">
+            <div class="article-card-front">
+                <img className="imagen" src={imagen} alt={titulo}/>
+            </div>
+            <div class="article-card-back">
+                <div class="title">
+                    <strong>
+                        <a className="link-title" onClick={handleClick}>
+                            {libro.titulo}
+                        </a>
+                    </strong>
+                </div>
+                <div class="autor">
+                    {libro.autor}
+                </div>
+                <div className= "description">
+                    {libro.descripcion}
+                </div>
+            </div>
+            
+        </div>
+    </div>
+          )
   }
   
   export default ArticleMiniature;
