46d3e5cc0b2c24887cc91bdb924bb357b14709fb
franarci <fran.arciniega96@gmail.com>
2021-05-26 17:26:00 -0300

    steps para addBook- form

--- web/adbooksweb-app/features/steps/clicks.js
+++ web/adbooksweb-app/features/steps/clicks.js
@@ -1,7 +1,9 @@
 const { When } = require("@cucumber/cucumber");
 
 When("I click the {string} button", async function(string){
-    const elem = await this.page.$(`[data-test="${string}"]`);
-    await elem.click();
-    return elem;
+
+    const btn = await this.page.$(`[data-test="${string}"]`);
+   
+    await btn.click()
+
 });
--- web/adbooksweb-app/features/steps/failImage.js
+++
@@ -1,6 +0,0 @@
-const { Then } = require("@cucumber/cucumber");
-
-Then("I should see a {string} message", async function({string}){
-    const elem = await this.page.waitForSelector(`[data-test="${string}"]`);
-    return !!elem;
-})
