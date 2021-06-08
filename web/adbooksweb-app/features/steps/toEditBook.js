ca4855d7a66eadf721f373f76eb06cb376c69c2f
franarci <fran.arciniega96@gmail.com>
2021-06-08 20:03:57 -0300

    cambio en step: given i navigate to x page

--- web/adbooksweb-app/features/steps/toEditBook.js
+++ web/adbooksweb-app/features/steps/toEditBook.js
@@ -2,4 +2,6 @@ const { Given } = require("@cucumber/cucumber");
 
 Given("I navigate to the {string} page", async function(string){
     await this.page.goto('http://localhost:3000/admin/edit?q=1')
+    const pageContainer = await this.page.$(`[data-test="${string}"]`) 
+    return !!pageContainer
 });
